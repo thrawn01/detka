d3dc368f6d3b4037f90425ba710e07988ae9cf04
Derrick J. Wippler <thrawn01@gmail.com>
2016-09-20 13:44:29 -0500

    Moved cmds so go compiles correctly

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -10,19 +10,18 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/braintree/manners"
-	"github.com/pressly/chi"
 	"github.com/thrawn01/args"
 	"github.com/thrawn01/detka"
 	"github.com/thrawn01/detka/kafka"
 	"github.com/thrawn01/detka/store"
-	"golang.org/x/net/context"
 )
 
 func main() {
-	parser := args.NewParser(args.Desc("Mail Workers for baby mailgun"),
-		args.EnvPrefix("WORKER_"))
+	parser := args.NewParser(
+		args.Desc("Public API endpoint for baby mailgun"),
+		args.EnvPrefix("API_"))
 	parser.AddOption("--bind").Alias("-b").Env("BIND").
-		Default("0.0.0.0:4141").Help("The interface to bind too")
+		Default("0.0.0.0:4040").Help("The interface to bind too")
 	parser.AddOption("--debug").Alias("-d").IsTrue().Env("DEBUG").
 		Help("Output debug messages")
 	parser.AddOption("--config").Alias("-c").Help("Read options from a config file")
@@ -43,51 +42,29 @@ func main() {
 	parser.AddOption("--rethink-auto-create").IsBool().Default("true").Env("RETHINK_AUTO_CREATE").
 		Help("Create db and tables if none exists")
 
-	// Decide which mail transport to use
-	parser.AddOption("--mail-transport").Alias("-M").Default("smtp").Env("MAIL_TRANSPORT").
-		Help("Choose what transport to use. choices('mailgun', 'smtp')")
-	parser.AddOption("--transport-retry").Alias("-R").Default("3").Env("TRANSPORT_RETRY").
-		Help("How many retries before giving up")
-
-	// Mailgun Options
-	parser.AddOption("--mailgun-domain").Env("MAILGUN_DOMAIN").Help("Mailgun Domain")
-	parser.AddOption("--mailgun-api-key").Env("MAILGUN_API_KEY").Help("Mailgun api-key")
-	parser.AddOption("--mailgun-public-key").Env("MAILGUN_PUBLIC_KEY").Help("Mailgun public-key")
-
-	// SMTP Options
-	parser.AddOption("--smtp-server").Env("SMTP_SERVER").Help("SMTP Server (mail.example.com:25)")
-	parser.AddOption("--smtp-user").Env("SMTP_USER").Help("SMTP User")
-	parser.AddOption("--smtp-password").Env("SMTP_PASSWORD").Help("SMTP Password")
-
 	opt := parser.ParseArgsSimple(nil)
 	if opt.Bool("debug") {
 		logrus.Info("Debug Enabled")
 		logrus.SetLevel(logrus.DebugLevel)
 	}
 
+	configFile := opt.String("config")
 	if opt.IsSet("config") {
-		content, err := detka.LoadFile(opt.String("config"))
+		content, err := detka.LoadFile(configFile)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to load config - %s\n", err.Error())
 			os.Exit(1)
 		}
 		opt, err = parser.FromIni(content)
+
 	}
 
-	mailer, err := detka.NewMailer(parser)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to init Mailer - %s\n", err.Error())
-		os.Exit(1)
-	}
-
-	dbStore := store.NewRethinkStore(parser)
-	consumerManager := kafka.NewConsumerManager(parser)
-
-	// Worker to handle messages from the event loop
-	worker := detka.NewWorker(consumerManager, dbStore, mailer)
+	// manages kafka connections
+	producerManager := kafka.NewProducerManager(parser)
+	// manages rethink connections
+	dbStore := store.NewRethinkStore(parser, nil)
 
 	if opt.IsSet("config") {
-		configFile := opt.String("config")
 		// Watch our config file for changes
 		cancelWatch, err := args.WatchFile(configFile, time.Second, func(err error) {
 			if err != nil {
@@ -98,27 +75,17 @@ func main() {
 			logrus.Info("Config file changed, Reloading...")
 			content, err := detka.LoadFile(configFile)
 			if err != nil {
-				logrus.Error("Failed to load config - ", err.Error())
+				logrus.Error("Failed to load config - %s\n", err.Error())
 				return
 			}
 			_, err = parser.FromIni(content)
 			if err != nil {
-				logrus.Info("Failed to update config - ", err.Error())
+				logrus.Info("Failed to update config - %s\n", err.Error())
 				return
 			}
 			// Perhaps our endpoints changed, we should reconnect
 			dbStore.SignalReconnect()
-			consumerManager.SignalReconnect()
-
-			// Perhaps our mailer config changed
-			mailer, err := detka.NewMailer(parser)
-			if err != nil {
-				logrus.Error("Failed to init Mailer - ", err.Error())
-				return
-			}
-			// Stop the current worker and create a new one
-			worker.Stop()
-			worker = detka.NewWorker(consumerManager, dbStore, mailer)
+			producerManager.Start()
 		})
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to watch '%s' -  %s", configFile, err.Error())
@@ -127,48 +94,24 @@ func main() {
 		defer cancelWatch()
 	}
 
-	// Serve up /healthz to indicate the health of our service
-	router := chi.NewRouter()
-	router.Get("/healthz", func(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
-		resp.Header().Set("Content-Type", "application/json; charset=utf-8")
-		notReady := func() {
-			resp.WriteHeader(500)
-			resp.Write([]byte(`{"ready" : false}`))
-		}
-
-		// Validate the health of kafka consumer
-		if !consumerManager.IsConnected() {
-			notReady()
-			return
-		}
-
-		// Is rethink connected?
-		if !dbStore.IsConnected() {
-			notReady()
-			return
-		}
-
-		resp.WriteHeader(200)
-		resp.Write([]byte(`{"ready" : true}`))
-	})
-	// TODO: Add metrics handler
-
 	server := manners.NewWithServer(&http.Server{
 		Addr:    opt.String("bind"),
-		Handler: router,
+		Handler: detka.NewHandler(producerManager, dbStore),
 	})
 
+	// Catch SIGINT Gracefully so we don't drop any active http requests
 	go func() {
 		signalChan := make(chan os.Signal, 1)
 		signal.Notify(signalChan, os.Interrupt, os.Kill)
 		sig := <-signalChan
 		logrus.Info(fmt.Sprintf("Captured %v. Exiting...", sig))
 		server.Close()
-		worker.Stop()
+		producerManager.Stop()
+		dbStore.Stop()
 	}()
 
 	logrus.Infof("Listening on %s...\n", opt.String("bind"))
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Server Error - %s\n", err.Error())
 		os.Exit(1)
--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -10,18 +10,19 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/braintree/manners"
+	"github.com/pressly/chi"
 	"github.com/thrawn01/args"
 	"github.com/thrawn01/detka"
 	"github.com/thrawn01/detka/kafka"
 	"github.com/thrawn01/detka/store"
+	"golang.org/x/net/context"
 )
 
 func main() {
-	parser := args.NewParser(
-		args.Desc("Public API endpoint for baby mailgun"),
-		args.EnvPrefix("API_"))
+	parser := args.NewParser(args.Desc("Mail Workers for baby mailgun"),
+		args.EnvPrefix("WORKER_"))
 	parser.AddOption("--bind").Alias("-b").Env("BIND").
-		Default("0.0.0.0:4040").Help("The interface to bind too")
+		Default("0.0.0.0:4141").Help("The interface to bind too")
 	parser.AddOption("--debug").Alias("-d").IsTrue().Env("DEBUG").
 		Help("Output debug messages")
 	parser.AddOption("--config").Alias("-c").Help("Read options from a config file")
@@ -42,29 +43,51 @@ func main() {
 	parser.AddOption("--rethink-auto-create").IsBool().Default("true").Env("RETHINK_AUTO_CREATE").
 		Help("Create db and tables if none exists")
 
+	// Decide which mail transport to use
+	parser.AddOption("--mail-transport").Alias("-M").Default("smtp").Env("MAIL_TRANSPORT").
+		Help("Choose what transport to use. choices('mailgun', 'smtp')")
+	parser.AddOption("--transport-retry").Alias("-R").Default("3").Env("TRANSPORT_RETRY").
+		Help("How many retries before giving up")
+
+	// Mailgun Options
+	parser.AddOption("--mailgun-domain").Env("MAILGUN_DOMAIN").Help("Mailgun Domain")
+	parser.AddOption("--mailgun-api-key").Env("MAILGUN_API_KEY").Help("Mailgun api-key")
+	parser.AddOption("--mailgun-public-key").Env("MAILGUN_PUBLIC_KEY").Help("Mailgun public-key")
+
+	// SMTP Options
+	parser.AddOption("--smtp-server").Env("SMTP_SERVER").Help("SMTP Server (mail.example.com:25)")
+	parser.AddOption("--smtp-user").Env("SMTP_USER").Help("SMTP User")
+	parser.AddOption("--smtp-password").Env("SMTP_PASSWORD").Help("SMTP Password")
+
 	opt := parser.ParseArgsSimple(nil)
 	if opt.Bool("debug") {
 		logrus.Info("Debug Enabled")
 		logrus.SetLevel(logrus.DebugLevel)
 	}
 
-	configFile := opt.String("config")
 	if opt.IsSet("config") {
-		content, err := detka.LoadFile(configFile)
+		content, err := detka.LoadFile(opt.String("config"))
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to load config - %s\n", err.Error())
 			os.Exit(1)
 		}
 		opt, err = parser.FromIni(content)
-
 	}
 
-	// manages kafka connections
-	producerManager := kafka.NewProducerManager(parser)
-	// manages rethink connections
+	mailer, err := detka.NewMailer(parser)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to init Mailer - %s\n", err.Error())
+		os.Exit(1)
+	}
+
 	dbStore := store.NewRethinkStore(parser, nil)
+	consumerManager := kafka.NewConsumerManager(parser)
+
+	// Worker to handle messages from the event loop
+	worker := detka.NewWorker(consumerManager, dbStore, mailer)
 
 	if opt.IsSet("config") {
+		configFile := opt.String("config")
 		// Watch our config file for changes
 		cancelWatch, err := args.WatchFile(configFile, time.Second, func(err error) {
 			if err != nil {
@@ -75,17 +98,27 @@ func main() {
 			logrus.Info("Config file changed, Reloading...")
 			content, err := detka.LoadFile(configFile)
 			if err != nil {
-				logrus.Error("Failed to load config - %s\n", err.Error())
+				logrus.Error("Failed to load config - ", err.Error())
 				return
 			}
 			_, err = parser.FromIni(content)
 			if err != nil {
-				logrus.Info("Failed to update config - %s\n", err.Error())
+				logrus.Info("Failed to update config - ", err.Error())
 				return
 			}
 			// Perhaps our endpoints changed, we should reconnect
 			dbStore.SignalReconnect()
-			producerManager.SignalReconnect()
+			consumerManager.Start()
+
+			// Perhaps our mailer config changed
+			mailer, err := detka.NewMailer(parser)
+			if err != nil {
+				logrus.Error("Failed to init Mailer - ", err.Error())
+				return
+			}
+			// Stop the current worker and create a new one
+			worker.Stop()
+			worker = detka.NewWorker(consumerManager, dbStore, mailer)
 		})
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Failed to watch '%s' -  %s", configFile, err.Error())
@@ -94,24 +127,48 @@ func main() {
 		defer cancelWatch()
 	}
 
+	// Serve up /healthz to indicate the health of our service
+	router := chi.NewRouter()
+	router.Get("/healthz", func(ctx context.Context, resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Set("Content-Type", "application/json; charset=utf-8")
+		notReady := func() {
+			resp.WriteHeader(500)
+			resp.Write([]byte(`{"ready" : false}`))
+		}
+
+		// Validate the health of kafka consumer
+		if !consumerManager.IsConnected() {
+			notReady()
+			return
+		}
+
+		// Is rethink connected?
+		if !dbStore.IsConnected() {
+			notReady()
+			return
+		}
+
+		resp.WriteHeader(200)
+		resp.Write([]byte(`{"ready" : true}`))
+	})
+	// TODO: Add metrics handler
+
 	server := manners.NewWithServer(&http.Server{
 		Addr:    opt.String("bind"),
-		Handler: detka.NewHandler(producerManager, dbStore),
+		Handler: router,
 	})
 
-	// Catch SIGINT Gracefully so we don't drop any active http requests
 	go func() {
 		signalChan := make(chan os.Signal, 1)
 		signal.Notify(signalChan, os.Interrupt, os.Kill)
 		sig := <-signalChan
 		logrus.Info(fmt.Sprintf("Captured %v. Exiting...", sig))
 		server.Close()
-		producerManager.Stop()
-		dbStore.Stop()
+		worker.Stop()
 	}()
 
 	logrus.Infof("Listening on %s...\n", opt.String("bind"))
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Server Error - %s\n", err.Error())
 		os.Exit(1)
